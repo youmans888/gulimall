94ade8ec1ddb3919a064aa829bf22e947e1f9168
LeeHappy <happy@git.com>
2021-05-28 21:48:32 +0800

    项目18

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,25 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//整合mybatis-Plus
+//1导入依赖
+/* <dependency>
+            <groupId>com.baomidou</groupId>
+            <artifactId>mybatis-plus-boot-starter</artifactId>
+            <version>3.3.1</version>
+       </dependency>
 
+2)配置
+    1配置数据源
+        1导入数据库的驱动
+        2在application.yml配置数据源相关信息
+    2配置mybatis-Plus
+        1使用@MapperScan
+        2告诉Mybatis-Plus,sql映射文件位置
+ */
+@MapperScan("com/atguigu/gulimall/product/dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,43 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
-
+    @Autowired
+    BrandService brandService ;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("huawei");
+        brandEntity.setName("华为");
+        brandService.save(brandEntity) ;
+        System.out.println("保存成功。。。");
+    }
+    @Test
+    void contextLoads2() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1l);
+        brandEntity.setDescript("华为手机");
+        brandService.updateById(brandEntity) ;
+        System.out.println("修改成功。。。");
+    }
+    @Test
+    void select() {
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1l));
+        list.forEach(brandEntity -> {
+            System.out.println(brandEntity);
+        });
     }
 
 }
