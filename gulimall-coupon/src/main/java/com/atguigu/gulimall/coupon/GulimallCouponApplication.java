837d1fa99ddfbfab73b6a7104bbb20ccdbe16c43
LeeHappy <happy@git.com>
2021-05-29 23:41:59 +0800

    p25完结

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -3,7 +3,26 @@ package com.atguigu.gulimall.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+/*
+1=如何使用nacos作为配置中心统一管理配置
+    1-引入依赖nacos-config
+    2-创建一个bootstrap.propertis配置文件
+        spring.application.name=gulimall-coupon
+        spring.cloud.nacos.config.server-addr=127.0.0.1:8848
+    3-需要给配置中心默认添加一个数据集，为应用名.properties ;gulimall-coupon.properties;
+    4-数据集中添加需要的配置
+    5-动态获取配置
+    控制器中@RefreshScope动态获取刷新配置
+    @Value注解配置
+    @Value("${coupon.user.name}")
+    优先使用配置中心的数据
+2=细节
+    1-命名空间隔离
+        1默认public（保留空间），默认所有的配置都在public空间
+    2
+    3
+    4
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,21 @@ import com.atguigu.common.utils.R;
  * @email 971572657@qq.com
  * @date 2021-05-28 21:53:27
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+    @Value("${coupon.user.name}")
+    private String name ;
+    @Value("${coupon.user.age}")
+    private String age ;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age) ;
+    }
 
     @RequestMapping("/member/list")
     public R membercoupons(){
